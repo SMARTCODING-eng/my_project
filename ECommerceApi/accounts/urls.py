9dece2135b40648d18256997266d96e4b07e1e46
Elutayo <elutayojohnson@gmail.com>
2025-08-31 16:24:31 +0100

    adding

--- ECommerceApi/accounts/urls.py
+++ ECommerceApi/accounts/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', accounts_home, name='user'),
+    # path('', accounts_home, name='user'),
     path('register/', RegisterView.as_view(template_name='accounts/register.html'), name='register'),
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
