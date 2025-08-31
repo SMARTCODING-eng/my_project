a5bf2067ff2af8bd8ba8554fe39a193313278e43
Elutayo <elutayojohnson@gmail.com>
2025-08-31 16:47:22 +0100

    adding

--- ECommerceApi/accounts/urls.py
+++ ECommerceApi/accounts/urls.py
@@ -3,20 +3,13 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'register', RegisterView, basename='register')
-router.register(r'login', LoginView, basename='login')
-router.register(r'logout', LogoutView, basename='logout')
-
+router.register(r'users', UserViewSet), basename='user'
 urlpatterns = [
-    path('accounts', include(router.urls)),
-    # path('', accounts_home, name='user'),
-    # path('accounts/register/', RegisterView.as_view(), name='register'),
-    # path('accounts/login/', LoginView.as_view(), name='login'),
-    # path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('', include(router.urls)),
+    path('accounts/register/', RegisterView.as_view(), name='register'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path('api-auth/', include('rest_framework.urls')),
 
 
-    
-]
-
+    ]
