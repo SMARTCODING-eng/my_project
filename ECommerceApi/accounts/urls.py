14a729b1c62f973f6e8781f40dda6b88a8d814f8
Elutayo <elutayojohnson@gmail.com>
2025-08-31 23:51:37 +0100

    adding README.md

--- ECommerceApi/accounts/urls.py
+++ ECommerceApi/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
-# from rest_framework.documentation import include_docs_urls
 
+# router registration  for userviewset
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
@@ -12,7 +12,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    # path('docs/', include_docs_urls(title='User API Documentation')),
     path('api-auth/', include('rest_framework.urls')),
     
 
